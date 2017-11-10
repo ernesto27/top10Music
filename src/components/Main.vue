98d166672fc109388dd2fa581b8901c56029bbba
Ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-11-10 17:24:09 -0300

    Agrego simple modal

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -34,16 +34,59 @@
 
       <section class="container" style="margin-top:30px">
         <div>
-            <h1>Selecciona tus discos favoritos</h1>
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="text" placeholder="Text input" v-model="q">
-                </div>
-            </div>
 
-            <ul>
-                <li v-for="result in results" v-text="result.title"></li>
-            </ul>
+            <a class="button is-primary" @click="isOpenModal = true">Seleccionar disco</a>
+
+            <div class="modal is-activef " v-bind:class="{'is-active': isOpenModal}">
+              <div class="modal-background"></div>
+              <div class="modal-card">
+                <header class="modal-card-head">
+                  <p class="modal-card-title">Selecciona un disco</p>
+                  <button class="delete" aria-label="close" @click='isOpenModal = false'></button>
+                </header>
+                <section class="modal-card-body">
+                    <div class="autocomplete-input">
+                        <p class="control has-icon has-icon-right">
+                            <input
+                                class="input is-large"
+                                placeholder="Search..."
+                                v-model="q"
+                            >
+                        </p>
+                        
+                        <ul class="options-list" style="margin-top: 2px;">
+                            <li class="highlighted" v-for="result in results">
+                                <article class="media">
+                                    <figure class="media-left">
+                                        <p class="image is-64x64">
+                                            <img :src="result.thumb">
+                                        </p>
+                                    </figure> 
+                                    <p>
+                                        <strong>{{result.title}}</strong> 
+                                        <br>
+                                    </p>
+                                </article>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <select>
+                        <option value="">10</option>
+                        <option value="">10</option>
+                        <option value="">10</option>
+                    </select>
+                </section>
+
+
+                <footer class="modal-card-foot">
+                  <button class="button is-success">Save changes</button>
+                  <button class="button" @click='isOpenModal = false'>Cancel</button>
+                </footer>
+              </div>
+            </div>
+            
+            
 
             <!-- Listado de albumes seleccionados - placeholder -->
 
@@ -94,18 +137,7 @@
 
 
     </div>
-
-<!--     <div>
-        <h1>Main page</h1>
-
-        <input type="text" v-model="q"/>
-
-        <ul>
-            <li v-for="result in results" v-text="result.title"></li>
-        </ul>
-    </div> -->
-
-    
+ 
 </template>
 
 
@@ -121,15 +153,16 @@
 
         data(){
             return{
-                q: 'the war',
-                results: [{}]
+                q: '',
+                results: [],
+                isOpenModal: false
             }
         },
 
         created(){
             console.log('Component created')
             console.log(api)
-            this.getResults();
+            // this.getResults();
             
         },
 
@@ -161,6 +194,10 @@
 
                         that.results = data;
                     });
+            },
+
+            openModal(){
+
             }
         },
 
@@ -175,3 +212,75 @@
     }
 
 </script>
+
+
+
+<style>
+
+
+
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+
+
+.autocomplete-input {
+  position: relative;
+  height: 300px;
+}
+
+ul.options-list {
+  display: flex;
+  flex-direction: column;
+  margin-top: -12px;
+  border: 1px solid #dbdbdb;
+  border-radius: 0 0 3px 3px;
+  position: absolute;
+  width: 100%;
+  overflow: hidden;
+}
+
+ul.options-list li {
+  width: 100%;
+  flex-wrap: wrap;
+  background: white;
+  margin: 0;
+  border-bottom: 1px solid #eee;
+  color: #363636;
+  padding: 7px;
+  cursor: pointer;
+}
+
+ul.options-list li.highlighted {
+  background: #f8f8f8
+}
+
+
+/** LIGHTBOX MARKUP **/
+
+.lightbox {
+    /** Default lightbox to hidden */
+    display: block;
+
+    /** Position and style */
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    top: 0;
+    left: 0;
+    background: rgba(56, 56, 56, 0.8);
+}
+
+
+
+</style>

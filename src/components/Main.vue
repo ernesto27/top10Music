80fcdcac058cf841331b74d05a02fe8ee0409263
ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-11-09 23:55:40 -0300

    Init repo

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <h1>Main page</h1>
+
+</template>
+
+
+<script>
+
+    import axios from 'axios'
+
+    export default{
+        name: 'main',
+
+        created: function(){
+            console.log('Component created')
+
+            axios.get('https://api.discogs.com/database/search?q=steven%20wi&year=2017&format=album&key=CqxjBdycyAtyPcujhrDo&secret=TMnbpRilxEtsesiQklgRaAZDRiOAeBqO')
+                .then(function(response){
+                    console.log(response)
+                })
+        }
+    }
+
+</script>

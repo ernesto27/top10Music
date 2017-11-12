485977ea21ab6532b190fadc528f0b501d98cc22
ernesto ponce <ernestoponce27@gmail.com>
2017-11-12 17:58:19 -0300

    Agrego componente de listado de albumes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -59,33 +59,12 @@
             </div>
 
             <!-- Listado de albumes seleccionados - placeholder -->
-
             <div>
                 <br />
                 <p class="title is-4">My ranking</p>
                 <br />
 
-
-                <div class="box" v-for="(album, index) in selectedAlbums">
-                  <article class="media">
-                      <span class="tag is-info is-rounded is-medium" style="margin-right: 6px;">{{index + 1}}</span>
-
-                    <div class="media-left">
-                      <figure class="image is-64x64">
-                        <img :src="album.thumb" alt="Image">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <div class="content">
-                        <p>
-                          <strong>{{album.title}}</strong>
-                          <br>
-                          {{album.year}}
-                        </p>
-                      </div>
-                    </div>
-                  </article>
-                </div>
+                <ranking-list :albums="selectedAlbums"></ranking-list>
             </div>
         </div>
 
---
+++ src/components/RankingList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="box" v-for="(album, index) in albums">
+          <article class="media">
+              <span class="tag is-info is-rounded is-medium" style="margin-right: 6px;">{{index + 1}}</span>
+
+            <div class="media-left">
+              <figure class="image is-64x64">
+                <img :src="album.thumb" alt="Image">
+              </figure>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>{{album.title}}</strong>
+                  <br>
+                  {{album.year}}
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        props: ['albums'],
+
+        created(){
+            console.log(this.albums)
+        }
+    }
+
+</script>
